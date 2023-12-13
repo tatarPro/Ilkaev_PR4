0d0e2de739acfa196a98b56a5a4cc4e3bcbe1f88
tatarPro <ilkaevnail3@gmail.com>
2023-12-13 21:32:41 +0500

    отредактировали задания

--- zadanie2.py
+++ zadanie2.py
@@ -1,6 +1,23 @@
 from math import sqrt
+from math import pi
 a = int(input("Введите сторону квадрата"))
 s = a**2
 p = a * 4
 d = a * sqrt(2)
 print(f"Площадь: {s}, периметр: {p}, диагональ: {d}")
+okruzh = input("Введите 1 если окружность описанная или 2 если вписанная")
+if okruzh == "1":
+    sokr = round(pi * (d/2)**2)
+    l = round(pi * (d/2) * 2)
+    stortreu = (d/2) * sqrt(3)
+    streu = ((stortreu**2) * sqrt(3)) / 4
+    print(f"Площадь описанной вокруг квадрата окружности равна: {sokr}, длина окружности: {l}, площадь треугольника: {streu}")
+elif okruzh == "2":
+    sokr = round(pi) * (a/2)**2
+    l = round(pi * (a/2) * 2)
+    osno = int(input("Введите основание"))
+    h = (a/2) * 3
+    streu = (h * osno) / 2
+    print(f"Площадь описанной вокруг квадрата окружности равна: {sokr}, длина окружности: {l}, площадь треугольника: {streu}")
+else:
+    print("Ошибка")
--- zadanie3.py
+++ zadanie3.py
@@ -1,5 +1,19 @@
 from math import pi
+from math import sqrt
 rad = int(input("Введите радиус круга: "))
 s = round(pi * rad**2)
 l = round(2 * pi * rad)
 print(f"Площадь: {s}, длина окружности: {l}")
+kvadrat = input("Введите 1 если квадрат описанный или 2 если вписанный")
+if kvadrat == "1":
+    stor = rad * 2
+    skvad = stor ** 2
+    pkvad = stor * 4
+    print(f"Площадь: {skvad}, периметр: {pkvad}")
+elif kvadrat == "2":
+    stor = rad / sqrt(2)
+    skvad = stor ** 2
+    pkvad = stor * 4
+    print(f"Площадь: {skvad}, периметр: {pkvad}")
+else:
+    print("Error")
